9ae0ed7764db6c724ea3eac9170f99e9a4ce2185
Graham Cox <graham@grahamcox.co.uk>
2013-06-16 18:43:29 +0100

    Able to install the header file

---
+++ include/attest/attest.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  attest.h
+ *
+ *    Description:  Main attest header file
+ *
+ *        Version:  1.0
+ *        Created:  16/06/13 18:33:53
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Graham Cox (coxg), graham@grahamcox.co.uk
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+
