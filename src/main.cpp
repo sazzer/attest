9cf5f318db8ee91211e39ac64abbdc963dd91f6b
Graham Cox <graham@grahamcox.co.uk>
2013-06-16 18:27:21 +0100

    More autotools work

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  The main entrypoint into running the tests
+ *
+ *        Version:  1.0
+ *        Created:  16/06/13 18:22:35
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Graham Cox (coxg), graham@grahamcox.co.uk
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+int main(void) {
+}
